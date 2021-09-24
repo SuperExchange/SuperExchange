202f661efd9811f3e1f06ab0859e94e3fb987d0c
DevilRaider-Dev <deviltwix3@gmail.com>
2021-09-24 17:08:15 +0200

    add dashboard crypto comp

--- src/components/DashCryptoItem.jsx
+++ src/components/DashCryptoItem.jsx
@@ -1,15 +1,29 @@
+import React, { Component } from 'react';
+
 class DashCryptoItem extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            name: props.name,
+            exchange: props.exchange
+        }
     }
-    render() { 
-        return ( 
-            <div>
-                
+    render() {
+        return (
+            <div className="DashStockElement">
+                <div className="DashStockGrid">
+                    <p>{this.state.name}</p>
+                    <p>
+                        {Number(this.state.exchange)
+                            .toFixed(2)
+                            .toLocaleString()}{" "}
+                        &nbsp;%
+                    </p>
+                </div>
             </div>
-         );
+        );
     }
+    //name, price, change(24h)
 }
- 
+
 export default DashCryptoItem;
