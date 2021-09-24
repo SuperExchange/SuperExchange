84835bf467dd05fa4d3073d3eb2e8e749dc7fcda
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-09-24 13:27:16 +0200

    ./src/data/stockDay.json created

--- src/components/DashStockElement.jsx
+++ src/components/DashStockElement.jsx
@@ -5,6 +5,7 @@ import watchListData from "../data/watchList.json";
 // const Search = "tesla";
 
 class DashStockElement extends React.Component {
+    //************************************************************************
     constructor(props) {
         super(props);
 
@@ -15,35 +16,40 @@ class DashStockElement extends React.Component {
             count: props.count,
             price: 0,
             change: 0,
-            stockData: {},
+            globalQuote: {},
         };
     }
 
-    /*
-    {
-        "Global Quote": {
-            "01. symbol": "BLK",
-            "02. open": "856.4500",
-            "03. high": "858.9789",
-            "04. low": "847.8100",
-            "05. price": "851.0500",
-            "06. volume": "544222",
-            "07. latest trading day": "2021-09-22",
-            "08. previous close": "845.2400",
-            "09. change": "5.8100",
-            "10. change percent": "0.6874%"
-        }
-    },
-     */
+    //************************************************************************
+    // sample format for an element, either offline or from api
+    // {
+    //     "Global Quote": {
+    //         "01. symbol": "BLK",
+    //         "02. open": "856.4500",
+    //         "03. high": "858.9789",
+    //         "04. low": "847.8100",
+    //         "05. price": "851.0500",
+    //         "06. volume": "544222",
+    //         "07. latest trading day": "2021-09-22",
+    //         "08. previous close": "845.2400",
+    //         "09. change": "5.8100",
+    //         "10. change percent": "0.6874%"
+    //     }
+    // },
 
+    //************************************************************************
     componentDidMount() {
         // fetch(
-        //     // `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${this.state.searchFor}&apikey=${ApiKey}`
-        //     `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${Search}&apikey=${ApiKey}`
+        //     `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${this.state.symbol}&apikey=${ApiKey}`
         // )
         //     .then((response) => response.json())
-        //     .then((json) => this.setState({ element: json }));
+        //     .then((json) => {
+        //         this.setState({ globalQuote: json }, () =>
+        //             this.setState({ loaded: true })
+        //         );
+        //     });
 
+        //--------------------------------------------------------------------
         // this is the offline version
         let index = watchListData.findIndex(
             (element) =>
@@ -51,7 +57,7 @@ class DashStockElement extends React.Component {
         );
         if (index < 0) return;
 
-        let element = watchListData[index][["Global Quote"]];
+        let element = watchListData[index]["Global Quote"];
         this.setState({ price: element["05. price"] });
         this.setState({ change: element["10. change percent"].split("%")[0] });
         this.setState({ loaded: true });
