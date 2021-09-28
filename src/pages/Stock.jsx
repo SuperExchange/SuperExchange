b4c89e77dee0db2869aea1c0d136bf66588a373f
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-09-28 09:31:41 +0200

    Small changes

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -2,7 +2,7 @@ import "../css/Stock.css";
 
 import React, { Component } from "react";
 
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 // import { useHistory } from "react-router-dom";
 
 import PlotlyParams from "../components/PlotlyParams";
@@ -13,11 +13,13 @@ import stockDay from "../data/stockDayList.json";
 import convertTimeSeries2Array from "../modules/convertTimeSeries2Array";
 import buildPlotlyDataFromTimeSeries from "../modules/buildPlotlyDataFromTimeSeries";
 
+const ApiKey = "ZCTSJTWC75K141H9";
+
 class Stock extends Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
-        console.log(this.props.match.params.symbol);
+        // console.log(this.props);
+        // console.log(this.props.match.params.symbol);
 
         this.state = {
             symbol: this.props.match.params.symbol,
@@ -28,54 +30,81 @@ class Stock extends Component {
         };
     }
 
-    handleOnChange = () => {
-        console.log(document.getElementById("txtSearch").value);
-    };
+    handleOnChange(e) {
+        // console.log(e.target.value);
+        // this.setState({ searchText: e.target.value });
+    }
 
-    handleOnClick = () => {
-        console.log(document.getElementById("txtSearch").value);
-    };
+    // starts the search
+    handleOnClick() {
+        let searchText = document.getElementById("txtSearch").value;
+
+        // make an api call to search a symbol by company name
+        fetch(
+            `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchText}&apikey=${ApiKey}`
+        )
+            .then((response) => response.json())
+            .then((json) => json.bestMatches)
+            .then((bestMatches) =>
+                bestMatches.length > 0 ? bestMatches[0] : null
+            )
+            .then((found) => this.setState({ symbol: found["1. symbol"] }))
+
+            .catch((err) => console.log(err));
+
+        // make an api call to get the data...
+        this.setState({ loaded: false });
+        fetch(
+            `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.state.symbol}&interval=30min&apikey=${ApiKey}`
+        )
+            .then((response) => response.json())
+            .then((json) => json.bestMatches)
+
+            .then()
+            .catch((err) => console.log(err));
+    }
 
     componentDidMount() {
+        // check if the symbol is in the watchlist
         let index = watchList.findIndex(
             (elt) => elt.symbol === this.state.symbol
         );
 
-        //  if we are offline
+        // if we are offline and the symbol is not in the watchlist, use the first one from watchlist
         index = Math.max(index, 0);
 
+        let timeSeries;
         if (index > -1) {
             this.setState({ watchListEntry: watchList[index] });
-        } else {
-            // make an api call to get the data...
+            timeSeries = convertTimeSeries2Array(stockDay[index]);
         }
+        if (timeSeries === null) return;
 
-        let timeSeries = convertTimeSeries2Array(stockDay[index]);
         let plotlyData = buildPlotlyDataFromTimeSeries(timeSeries);
-        console.log(timeSeries);
-        console.log(plotlyData);
+        // console.log(timeSeries);
+        // console.log(plotlyData);
 
         this.setState({ timeSeries: timeSeries });
         this.setState({ plotlyData: plotlyData });
         this.setState({ loaded: true });
     }
 
+    shouldComponentUpdate() {
+        return true;
+    }
+
     render() {
         // console.log(this.state.watchListEntry);
         return (
-            <div>
-                <h1>Stock</h1>
-                <p>
-                    With all of the styling tool options available in today’s
-                    market
-                </p>
+            <div className="Stock">
                 <div className="StockHeader">
-                    <h2>
-                        {this.state.loaded &&
-                            `${this.state.watchListEntry.name} (${this.state.watchListEntry.symbol})`}
-                        {!this.state.loaded && "No data available"}
-                    </h2>
-
+                    <div className="StockHeaderTitle">
+                        <h1>Stock</h1>
+                        <p>
+                            With all of the styling tool options available in
+                            today’s market
+                        </p>
+                    </div>
                     <div className="StockHeaderSearch">
                         <input
                             type="text"
@@ -90,6 +119,13 @@ class Stock extends Component {
                     </div>
                 </div>
 
+                <div className="StockData">
+                    <h2>
+                        {this.state.loaded &&
+                            `${this.state.watchListEntry.name} (${this.state.watchListEntry.symbol})`}
+                        {!this.state.loaded && "No data available"}
+                    </h2>
+                </div>
                 {this.state.loaded && (
                     <PlotlyParams
                         lastRefreshed={
@@ -102,7 +138,7 @@ class Stock extends Component {
                         data={this.state.plotlyData}
                     />
                 )}
-            </div>
+            </div> // end
         );
     }
 }
